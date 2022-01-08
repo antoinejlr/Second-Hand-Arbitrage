f0a5ff72acbc756750f20afb17f6e86189156458
antoinejlr <antoine.jl.rousseau@gmail.com>
2022-01-08 18:23:03 +0100

    minor refactoring of src files

--- src/product_page_desc_scraper.py
+++ src/product_page_desc_scraper.py
@@ -1,13 +1,14 @@
 # fetches the description a product page
 import csv
 import glob
-import time
+import os
 
 import pandas as pd
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from tqdm import tqdm
+from pathlib import Path
 
 options = webdriver.FirefoxOptions()
 options.headless = True
@@ -40,21 +41,26 @@ def fetch_page_source(branch_url: str) -> str:
     return pageSource
 
 
-def fetch_product_desc_from_str(html: str) -> str:
+def url_to_file_name(url: str) -> str:
     """
-    fetches the product description if given the html as string
-    :param html: product page html as string
-    :return: product description string
+    changes url into a valid file name
+    :param url: branch url
+    :return: branch url as a valid file name
     """
-    bs = BeautifulSoup(html, "html.parser")
-    description = (
-        bs.div.div.section.contents[-4]
-        .div.div.div.contents[3]
-        .div.div.div.section.contents[2]
-        .div.div.contents[2]
-        .article.p.text
-    )
-    return description
+    file_name_string = "".join(i for i in url if i not in "/:*?<>|\\")
+    return file_name_string
+
+
+def save_page_source(file_name: str, content: str) -> None:
+    """
+    saves content with file_name in the html_product folder
+    :param file_name: valid file name
+    :param content: page source
+    :return: None
+    """
+    file = f"../html_product/{file_name}.html"
+    with open(file, "w") as f:
+        f.write(content)
 
 
 def fetch_product_desc_from_file(file_name: str) -> tuple:
@@ -66,91 +72,46 @@ def fetch_product_desc_from_file(file_name: str) -> tuple:
     file = f"../html_product/{file_name}.html"
     with open(file, "r") as f:
         bs = BeautifulSoup(f, "html.parser")
-        body_description = bs.div.div.section.contents[
-            1
-        ].div.div.div  # .contents[3].div.div.div.section.contents[
-        # 2].div.div.contents[-1].article.get_text(separator="\n", strip=True)
-        header_description = (
-            bs.div.div.section.contents[1]
-            .div.div.div.contents[3]
-            .div.div.div.section.contents[2]
-            .div.div.contents[-2]
-            .text
+        body_description = (
+            bs.div.div.section.contents[-4]
+                .div.div.div.contents[3]
+                .div.div.div.section.contents[-1]
+                .div.div.contents[-1]
+                .article.get_text(separator="\n", strip=True)
         )
+        try:
+            header_description = (
+                bs.div.div.section.contents[-4]
+                    .div.div.div.contents[3]
+                    .div.div.div.section.contents[-1]
+                    .div.div.h2.text
+            )
+        except AttributeError:
+            header_description = None
 
     return header_description, body_description
 
 
-def url_to_file_name(url):
-    # changes url into a valid file name
-    file_name_string = "".join(i for i in url if i not in "/:*?<>|\\")
-    return file_name_string
-
-
-def save_page_source(file_name, content):
-    file = f"../html_product/{file_name}.html"
-    with open(file, "w") as f:
-        f.write(content)
-
-
 def append_to_url_filename_description_mapping(
-    url, file_name, header_description, body_description
-):
+        url: str, file_name: str, header_description: str, body_description: str
+) -> None:
+    """
+    Adds a row to the url_filename_description_mapping csv containing
+    :param url: branch url of the product page
+    :param file_name: valid file_name based on url
+    :param header_description: product description header (sometimes does not exist)
+    :param body_description: product description body (always exists)
+    :return: None
+    """
     with open(
-        "/Users/Shared/github_projects/Second-Hand-Arbitrage/metadata/url_filename_description_mapping.csv",
-        "a",
+            "/Users/Shared/github_projects/Second-Hand-Arbitrage/metadata/url_filename_description_mapping.csv",
+            "a",
     ) as file:
         writer = csv.writer(file, quoting=csv.QUOTE_NONNUMERIC)
         writer.writerow([url, file_name, header_description, body_description])
 
 
-def append_url_to_list_of_urls(url):
-    with open(
-        "/Users/Shared/github_projects/Second-Hand-Arbitrage/metadata/product_page_urls.csv",
-        "a",
-    ) as file:
-        writer = csv.writer(
-            file, delimiter="\n", escapechar=" ", quoting=csv.QUOTE_NONE
-        )
-        writer.writerow([url])
-
-
-def load_product_pages(product_page_urls: list) -> None:
-    """
-    Step 1:
-    fetches all the product pages in list of urls
-
-    Step 2:
-    saves the html files
-
-    Step 3:
-    appends url, filename and product description to the mapping csv
-
-    Step 4:
-    append the newly capture product page urls to the list of urls for which a product page exist
-
-    :return: None
-    """
-    for product_page_url in tqdm(product_page_urls):
-        # Step 2
-        pageSource = fetch_page_source(product_page_url)
-        product_file_name = url_to_file_name(product_page_url)
-        save_page_source(product_file_name, pageSource)
-
-        # Step 3
-        header_description, body_description = fetch_product_desc_from_file(
-            product_file_name
-        )
-        append_to_url_filename_description_mapping(
-            product_page_url, product_file_name, header_description, body_description
-        )
-
-        # Step 4
-        append_url_to_list_of_urls(product_page_url)
-        time.sleep(2)
-
-
-def fetch_all_product_urls() -> list:
+def fetch_unique_product_urls() -> list:
     """
     isolates unique urls from all the csv files captured to date
     :return: list of all product urls
@@ -172,42 +133,53 @@ def fetch_all_product_urls() -> list:
     rejected_dates = csv_df_reject.index[csv_df_reject["url"] is True].tolist()
     csv_df_valid_dates = csv_df[~csv_df["date"].isin(rejected_dates)]
 
-    urls = list(set(csv_df_valid_dates.url.tolist()))
-    return urls
+    unique_product_urls = list(set(csv_df_valid_dates.url.tolist()))
+    return unique_product_urls
 
 
-def fetch_new_product_urls() -> list:
+def fetch_new_product_urls(unique_product_urls: list) -> list:
     """
     checks the list of urls for which a product page exists
     against the latest list of urls and fetches all the product pages not yet captured
     :return: None
     """
-    # Step 1
-    product_page_urls_path = "/Users/Shared/github_projects/Second-Hand-Arbitrage/metadata/product_page_urls.csv"
-    product_page_urls_df = pd.read_csv(product_page_urls_path, names=["url"])
-    latest_file_df = pd.DataFrame(fetch_all_product_urls(), columns=["url"])
-    new_product_page_urls = list(
+    csv_file_path_list = glob.glob(
+        "/Users/Shared/github_projects/Second-Hand-Arbitrage/html_product/*"
+    )
+    product_urls = [Path(full_path).stem for full_path in csv_file_path_list]
+    product_page_urls_df = pd.DataFrame(product_urls, columns=["file_url"])
+    unique_file_urls = list(map(url_to_file_name, unique_product_urls))
+    latest_file_df = pd.DataFrame(zip(unique_product_urls, unique_file_urls), columns=["url", "file_url"])
+    new_product_urls = list(
         set(
-            pd.merge(latest_file_df, product_page_urls_df, how="outer", indicator=True)
-            .query('_merge == "left_only"')
-            .drop(columns=["_merge"])["url"]
-            .to_list()
+            pd.merge(latest_file_df, product_page_urls_df, how="outer", on='file_url', indicator=True)
+                .query('_merge == "left_only"')
+                .drop(columns=["_merge"])["url"]
+                .to_list()
         )
     )
-    return new_product_page_urls
+    return new_product_urls
 
 
 def main():
-    urls = fetch_new_product_urls()
-    load_product_pages(urls)
+    unique_urls = fetch_unique_product_urls()  # get unique product urls by scanning all csv files
+    urls = fetch_new_product_urls(unique_urls)  # keep the url of product pages absent in html_product
+    for url in tqdm(urls):  # scrape page by page the product urls and save them to html_product
+        page_source = fetch_page_source(url)
+        valid_name = url_to_file_name(url)
+        save_page_source(valid_name, page_source)
+    os.remove("/Users/Shared/github_projects/Second-Hand-Arbitrage/metadata/url_filename_description_mapping.csv")
+    for url in tqdm(unique_urls):  # keep the
+        valid_name = url_to_file_name(url)
+        header_description, body_description = fetch_product_desc_from_file(
+            valid_name
+        )
+        append_to_url_filename_description_mapping(url, valid_name, header_description, body_description)
 
 
 if __name__ == "__main__":
-    # main()
-    print(fetch_product_desc_from_file("fracanon-ef-50mm-f-1.2l-usm-1187571954"))
+    main()
 
-    # to do --> clean up the Beautiful Soup selectors
-    # clean up the mapping file as a result and rerun
     # merge the original dataframe with the one with descriptions
     # use the descriptions to identify the language of the ad
     # harmonize the language of the ads
