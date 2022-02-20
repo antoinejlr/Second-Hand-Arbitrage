483fd73d552eca8370030b3cd80c9ac178ed7d63
antoinejlr <antoine.jl.rousseau@gmail.com>
2022-02-20 14:44:35 +0100

    updated data

--- src/orchestration.py
+++ src/orchestration.py
@@ -7,22 +7,52 @@ import listings_feature_engineering
 from model_training import FEATURES, PandasLabelEncoder
 import listings_with_prediction
 import listings_summary
+import socket
+import time
 
-flow = fetch_search_results_pages.main()
-if flow:
-    print("All search result pages fetched successfully")
-    flow = save_search_results_products.main()
-if flow:
-    print("Full search product pages saved to csv")
-    fetch_product_pages.main()
-    print("Product pages fetched successfully")
-    save_product_pages_details.main()
-    print("Unique listings fetched")
-    listings_cleansing.main()
-    print("Unique listings cleaned")
-    listings_feature_engineering.main()
-    print("Unique listing features created")
-    listings_with_prediction.main()
-    print("Unique listing products predicted")
-    listings_summary.main()
-    print("Unique listing with summary statistics saved")
+
+def isConnected():
+    try:
+        # connect to the host -- tells us if the host is actually
+        # reachable
+        sock = socket.create_connection(("www.google.com", 80))
+        if sock is not None:
+            sock.close
+        return True
+    except OSError:
+        pass
+    return False
+
+
+if __name__ == "__main__":
+    # try 5 times to connect
+    flow = False
+    for test in range(5):
+        if isConnected():
+            flow = True
+            break
+        print("Failed to connect, trying again in 10s")
+        time.sleep(10)
+
+    if flow:
+        print("Connection successful, starting script")
+        flow = fetch_search_results_pages.main()
+    else:
+        print('Failed to connect: aborting script')
+    if flow:
+        print("All search result pages fetched successfully")
+        flow = save_search_results_products.main()
+    if flow:
+        print("Full search product pages saved to csv")
+        fetch_product_pages.main()
+        print("Product pages fetched successfully")
+        save_product_pages_details.main()
+        print("Unique listings fetched")
+        listings_cleansing.main()
+        print("Unique listings cleaned")
+        listings_feature_engineering.main()
+        print("Unique listing features created")
+        listings_with_prediction.main()
+        print("Unique listing products predicted")
+        listings_summary.main()
+        print("Unique listing with summary statistics saved")
